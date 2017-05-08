e9b46dea986a2bfc8dfa732870537f5a6b24139e
Jefferson Rodrigues A. Sousa <jefferson.sousa@interfile.com.br>
2017-05-08 15:00:05 -0300

    Inclusão Classe Banco

---
+++ CaixaEletronico/Banco.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaEletronico
+{
+    class Banco
+    {
+        //string[] banco = new string[5];
+
+        //public void adiciona(string valor)
+        //{
+        //    banco[0] = valor
+        //}
+
+        private Conta[] contas = new Conta[10];
+
+        public int quantidade;
+
+        public void adiciona(Conta conta)
+        {
+            this.contas[this.quantidade] = conta;
+            this.quantidade++;
+        }
+    }
+}
